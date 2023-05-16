0ab03769e4476ad7b9955366271d8bed49e020f2
Rocco <rocco@master-chef.hitman>
2023-05-16 15:35:52 +0000

    Create Croupier files

--- src/Croupier.cpp
+++ src/Croupier.cpp
@@ -1,4 +1,4 @@
-#include "MyMod.h"
+#include "Croupier.h"
 
 #include <Logging.h>
 #include <IconsMaterialDesign.h>
@@ -7,48 +7,48 @@
 #include <Glacier/ZGameLoopManager.h>
 #include <Glacier/ZScene.h>
 
-void MyMod::OnEngineInitialized() {
-    Logger::Info("MyMod has been initialized!");
+void Croupier::OnEngineInitialized() {
+    Logger::Info("Croupier has been initialized!");
 
     // Register a function to be called on every game frame while the game is in play mode.
-    const ZMemberDelegate<MyMod, void(const SGameUpdateEvent&)> s_Delegate(this, &MyMod::OnFrameUpdate);
+    const ZMemberDelegate<Croupier, void(const SGameUpdateEvent&)> s_Delegate(this, &Croupier::OnFrameUpdate);
     Globals::GameLoopManager->RegisterFrameUpdate(s_Delegate, 1, EUpdateMode::eUpdatePlayMode);
 
     // Install a hook to print the name of the scene every time the game loads a new one.
-    Hooks::ZEntitySceneContext_LoadScene->AddDetour(this, &MyMod::OnLoadScene);
+    Hooks::ZEntitySceneContext_LoadScene->AddDetour(this, &Croupier::OnLoadScene);
 }
 
-MyMod::~MyMod() {
+Croupier::~Croupier() {
     // Unregister our frame update function when the mod unloads.
-    const ZMemberDelegate<MyMod, void(const SGameUpdateEvent&)> s_Delegate(this, &MyMod::OnFrameUpdate);
+    const ZMemberDelegate<Croupier, void(const SGameUpdateEvent&)> s_Delegate(this, &Croupier::OnFrameUpdate);
     Globals::GameLoopManager->UnregisterFrameUpdate(s_Delegate, 1, EUpdateMode::eUpdatePlayMode);
 }
 
-void MyMod::OnDrawMenu() {
+void Croupier::OnDrawMenu() {
     // Toggle our message when the user presses our button.
-    if (ImGui::Button(ICON_MD_LOCAL_FIRE_DEPARTMENT " MyMod")) {
+    if (ImGui::Button(ICON_MD_LOCAL_FIRE_DEPARTMENT " Croupier")) {
         m_ShowMessage = !m_ShowMessage;
     }
 }
 
-void MyMod::OnDrawUI(bool p_HasFocus) {
+void Croupier::OnDrawUI(bool p_HasFocus) {
     if (m_ShowMessage) {
         // Show a window for our mod.
-        if (ImGui::Begin("MyMod", &m_ShowMessage)) {
+        if (ImGui::Begin("Croupier", &m_ShowMessage)) {
             // Only show these when the window is expanded.
-            ImGui::Text("Hello from MyMod!");
+            ImGui::Text("Hello from Croupier!");
         }
         ImGui::End();
     }
 }
 
-void MyMod::OnFrameUpdate(const SGameUpdateEvent &p_UpdateEvent) {
+void Croupier::OnFrameUpdate(const SGameUpdateEvent &p_UpdateEvent) {
     // This function is called every frame while the game is in play mode.
 }
 
-DEFINE_PLUGIN_DETOUR(MyMod, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData) {
+DEFINE_PLUGIN_DETOUR(Croupier, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData) {
     Logger::Debug("Loading scene: {}", p_SceneData.m_sceneName);
     return HookResult<void>(HookAction::Continue());
 }
 
-DECLARE_ZHM_PLUGIN(MyMod);
+DECLARE_ZHM_PLUGIN(Croupier);
--- src/Croupier.h
+++ src/Croupier.h
@@ -4,19 +4,19 @@
 
 #include <Glacier/SGameUpdateEvent.h>
 
-class MyMod : public IPluginInterface {
+class Croupier : public IPluginInterface {
 public:
     void OnEngineInitialized() override;
-    ~MyMod() override;
+    ~Croupier() override;
     void OnDrawMenu() override;
     void OnDrawUI(bool p_HasFocus) override;
 
 private:
     void OnFrameUpdate(const SGameUpdateEvent& p_UpdateEvent);
-    DECLARE_PLUGIN_DETOUR(MyMod, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData);
+    DECLARE_PLUGIN_DETOUR(Croupier, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData);
 
 private:
     bool m_ShowMessage = false;
 };
 
-DEFINE_ZHM_PLUGIN(MyMod)
+DEFINE_ZHM_PLUGIN(Croupier)
