ad28d7cbe42c82b9f42a74e1a6272babfb71aa0c
Deji <the_zone@hotmail.co.uk>
2024-07-26 22:41:19 +0100

    Cleanup

--- app/TimerSettingsWindow.xaml.cs
+++ app/TimerSettingsWindow.xaml.cs
@@ -3,17 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Croupier {
 	public class TimerSettingsWindowViewModel : ViewModel {
