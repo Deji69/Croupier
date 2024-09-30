fa46c7a3a443fae4274db1c92611bdfb54de2043
Deji <the_zone@hotmail.co.uk>
2024-09-30 23:00:14 +0100

    Bump version

--- app/AssemblyInfo.cs
+++ app/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.11")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
-[assembly: AssemblyInformationalVersion("2.11")]
+[assembly: AssemblyVersion("2.11.1")]
+[assembly: AssemblyFileVersion("2.11.1.0")]
+[assembly: AssemblyInformationalVersion("2.11.1")]
 [assembly: AssemblyProduct("Croupier")]
 [assembly: AssemblyCopyright("Copyright © BluntsNBeatz, 2024")]
 [assembly: AssemblyDescription("Roulette spin generation tool for Hitman: World of Assassination.")]
