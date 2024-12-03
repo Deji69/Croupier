1855f61e04d4d99ba3bf6a6382c1705796957642
Deji <the_zone@hotmail.co.uk>
2024-12-03 20:59:50 +0000

    Cleanup

--- app/Config.cs
+++ app/Config.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Croupier {
 	class Config {
