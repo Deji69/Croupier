a352acd818100de22b32ea05922b86c1e338a470
Deji <the_zone@hotmail.co.uk>
2025-01-18 13:06:35 +0000

    Make RR16 the default ruleset

--- app/MainWindow.xaml.cs
+++ app/MainWindow.xaml.cs
@@ -802,7 +802,7 @@ namespace Croupier
 			}
 
 			var activeRuleset = rulesets.FirstOrDefault(r => r.Name == Config.Default.Ruleset)
-				?? rulesets.FirstOrDefault(r => r.Name == "RRWC 2024")
+				?? rulesets.FirstOrDefault(r => r.Name == "RR16")
 				?? rulesets.FirstOrDefault();
 
 			if (activeRuleset != null) {
