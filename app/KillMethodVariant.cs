0ae9ce9ccd96b5cd788b53a4487f027408f385c0
Deji <the_zone@hotmail.co.uk>
2025-03-20 04:36:59 +0000

    Null check fix

--- app/KillMethodVariant.cs
+++ app/KillMethodVariant.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Nodes;
 
 namespace Croupier {
 	public class KillMethodVariant(KillMethod method, string name, string? image = null, StringCollection? tags = null)
-		: KillMethod($"{name} {method.Name}", image ?? method.Image, method.Category, method.Target, [..method.Tags, ..tags], method.Keywords)
+		: KillMethod($"{name} {method.Name}", image ?? method.Image, method.Category, method.Target, [..method.Tags, ..(tags ?? [])], method.Keywords)
 	{
 		public KillMethod Method { get; private set; } = method;
 		public string VariantName { get; private set; } = name;
