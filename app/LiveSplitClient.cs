4a7f90d507d6b01f02376d8f7bbda83136f41c07
Deji <the_zone@hotmail.co.uk>
2026-06-21 22:25:27 +0100

    Switch LiveSplit client to pipe based communication

--- app/LiveSplitClient.cs
+++ app/LiveSplitClient.cs
@@ -29,14 +29,17 @@ namespace Croupier {
 			started = true;
 			connected = false;
 
-			//socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-			pipe = new NamedPipeClientStream("LiveSplit");
+			if (Config.Default.LiveSplitUseSocketServer)
+				socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+			else
+				pipe = new NamedPipeClientStream(".", "LiveSplit", PipeDirection.InOut, PipeOptions.Asynchronous);
 
 			Status("Connecting...");
 
 			while (!CancelConnection.IsCancellationRequested) {
 				try {
-					await pipe.ConnectAsync(5000, CancelConnection.Token);
+					if (pipe != null) await pipe.ConnectAsync(CancelConnection.Token);
+					else if (socket != null) await socket.ConnectAsync(Config.Default.LiveSplitIP, Config.Default.LiveSplitPort, CancelConnection.Token);
 				} catch (SocketException e) {
 					if (CancelConnection.IsCancellationRequested) break;
 					Status($"{e.Message}\nCheck LiveSplit Server is running (Control > Start Server) and that the IP and Port are correct.");
@@ -44,25 +47,27 @@ namespace Croupier {
 					await Task.Delay(5000);
 				}
 
-				if (!pipe.IsConnected)
+				if ((pipe != null && !pipe.IsConnected) || (socket != null && !socket.Connected))
 					continue;
 
-				Status("[LIVESPLIT] Connected.");
+				Status("Connected.");
 				connected = true;
 
-				while (pipe.IsConnected && !CancelConnection.IsCancellationRequested) {
+				while (((pipe != null && pipe.IsConnected) || (socket != null && socket.Connected)) && !CancelConnection.IsCancellationRequested) {
 					await Task.Delay(2000);
 				}
 
 				if (CancelConnection.IsCancellationRequested)
 					break;
 
-				Status($"[LIVESPLIT] Disconnected.");
+				Status("Disconnected.");
 				connected = false;
 			}
 
-			if (connected)
-				pipe.Dispose();
+			if (connected) {
+				pipe?.Dispose();
+				socket?.Disconnect(true);
+			}
 
 			connected = false;
 			started = false;
@@ -80,6 +85,14 @@ namespace Croupier {
 		}
 
 		public async Task<bool> Send(string command) {
+			if (pipe != null)
+				return await SendViaPipe(command);
+			if (socket != null)
+				return await SendViaSocket(command);
+			return false;
+		}
+
+		public async Task<bool> SendViaPipe(string command) {
 			if (!connected || pipe == null || !pipe.IsConnected)
 				return false;
 			try {
@@ -90,7 +103,26 @@ namespace Croupier {
 			return true;
 		}
 
+		public async Task<bool> SendViaSocket(string command) {
+			if (!connected || socket == null || !socket.Connected)
+				return false;
+			try {
+				await socket.SendAsync(Encoding.ASCII.GetBytes($"{command}\r\n"));
+			} catch {
+				return false;
+			}
+			return true;
+		}
+
 		public async Task<string?> Receive(string command) {
+			if (socket != null)
+				return await ReceiveViaSocket(command);
+			if (pipe != null)
+				return await ReceiveViaPipe(command);
+			return null;
+		}
+
+		public async Task<string?> ReceiveViaPipe(string command) {
 			if (!connected || pipe == null || !pipe.IsConnected)
 				return null;
 			var buffer = new byte[1024];
@@ -99,6 +131,23 @@ namespace Croupier {
 				return null;
 
 			var size = await pipe.ReadAsync(buffer);
+			if (size == 0)
+				return null;
+			var response = Encoding.ASCII.GetString(buffer, 0, size);
+			var resArr = response.Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+			var result = resArr.Last();
+			return result;
+		}
+
+		public async Task<string?> ReceiveViaSocket(string command) {
+			if (!connected || socket == null || !socket.Connected)
+				return null;
+			var buffer = new byte[1024];
+
+			if (!await this.Send(command))
+				return null;
+
+			var size = await socket.ReceiveAsync(buffer);
 			if (size == 0)
 				return null;
 			var response = Encoding.ASCII.GetString(buffer, 0, size);
